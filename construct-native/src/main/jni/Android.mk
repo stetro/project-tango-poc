a7f941332147e10325d9731337a7837c2ca4cce4
Steffen Tröster <steffen.troester@inovex.de>
2015-11-09 14:43:46 +0100

    replace plane fitting example and skip native build on travis

--- construct-native/src/main/jni/Android.mk
+++ construct-native/src/main/jni/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2014 Google Inc. All Rights Reserved.
+# Copyright 2015 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,46 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 LOCAL_PATH := $(call my-dir)
-PROJECT_ROOT_FROM_JNI:= ../../../../..
-PROJECT_ROOT:= $(call my-dir)/../../../../..
+PROJECT_ROOT_FROM_JNI := ../../../../..
+PROJECT_ROOT := $(LOCAL_PATH)/$(PROJECT_ROOT_FROM_JNI)
+MY_SOURCE_DIR := /home/stetro/Source/tango-examples-c
+
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := libaugmented_reality_jni_example
-LOCAL_SHARED_LIBRARIES := tango_client_api
-LOCAL_CFLAGS    := -std=c++11
-MY_LOCAL_LIB:= /home/stetro/Source/tango-examples-c
-
+LOCAL_MODULE := plane_fitting_jni_example
+LOCAL_SHARED_LIBRARIES := tango_client_api tango_support_api
+LOCAL_CFLAGS := -std=c++11
+LOCAL_C_INCLUDES := $(MY_SOURCE_DIR)/tango-gl/include \
+                    $(MY_SOURCE_DIR)/third-party/glm
 LOCAL_SRC_FILES := jni_interface.cc \
-                   point_cloud_data.cc \
-                   point_cloud_drawable.cc \
-                   augmented_reality_app.cc \
-                   pose_data.cc \
-                   scene.cc \
-                   tango_event_data.cc \
-                   $(MY_LOCAL_LIB)/tango-gl/axis.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/bounding_box.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/camera.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/conversions.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/drawable_object.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/frustum.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/gesture_camera.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/grid.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/goal_marker.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/line.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/mesh.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/shaders.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/trace.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/transform.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/util.cpp \
-                   $(MY_LOCAL_LIB)/tango-gl/video_overlay.cpp
-
-LOCAL_C_INCLUDES := $(MY_LOCAL_LIB)/tango-gl/include \
-                    $(MY_LOCAL_LIB)/third-party/glm/
-
-LOCAL_LDLIBS    := -llog -lGLESv2 -L$(SYSROOT)/usr/lib
+                   plane_fitting.cc \
+                   plane_fitting_application.cc \
+                   point_cloud.cc \
+                   $(MY_SOURCE_DIR)/tango-gl/bounding_box.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/camera.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/conversions.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/cube.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/drawable_object.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/mesh.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/shaders.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/transform.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/util.cpp \
+                   $(MY_SOURCE_DIR)/tango-gl/video_overlay.cpp
+LOCAL_LDLIBS := -lGLESv2 -llog -L$(SYSROOT)/usr/lib
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path, $(MY_LOCAL_LIB))
+$(call import-add-path,$(MY_SOURCE_DIR))
 $(call import-module,tango_client_api)
+$(call import-module,tango_support_api)
--- construct-native/src/main/jni/jni_interface.cc
+++ construct-native/src/main/jni/jni_interface.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Google Inc. All Rights Reserved.
+ * Copyright 2015 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,120 +14,70 @@
  * limitations under the License.
  */
 
-#define GLM_FORCE_RADIANS
-
 #include <jni.h>
-#include <tango-augmented-reality/augmented_reality_app.h>
 
-static tango_augmented_reality::AugmentedRealityApp app;
+#include "tango-plane-fitting/plane_fitting_application.h"
+
+static tango_plane_fitting::PlaneFittingApplication app;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-jint JNI_OnLoad(JavaVM* vm, void*) {
-  // We need to store a reference to the Java VM so that we can call into the
-  // Java layer to trigger rendering.
-  app.SetJavaVM(vm);
-  return JNI_VERSION_1_6;
-}
 
 JNIEXPORT jint JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_initialize(
-    JNIEnv* env, jobject, jobject activity) {
+Java_de_stetro_master_constructnative_JNIInterface_tangoInitialize(
+    JNIEnv* env, jobject /*obj*/, jobject activity) {
   return app.TangoInitialize(env, activity);
 }
 
 JNIEXPORT jint JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_setupConfig(
-    JNIEnv*, jobject) {
-  return app.TangoSetupConfig();
-}
-
-JNIEXPORT jint JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_connect(
-    JNIEnv*, jobject) {
-  return app.TangoConnect();
-}
-
-JNIEXPORT jint JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_connectCallbacks(
-    JNIEnv*, jobject) {
-  int ret = app.TangoConnectCallbacks();
-  return ret;
+Java_de_stetro_master_constructnative_JNIInterface_tangoSetupAndConnect(
+    JNIEnv* /*env*/, jobject /*obj*/) {
+  return app.TangoSetupAndConnect();
 }
 
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_disconnect(
-    JNIEnv*, jobject) {
+Java_de_stetro_master_constructnative_JNIInterface_tangoDisconnect(
+    JNIEnv* /*env*/, jobject /*obj*/) {
   app.TangoDisconnect();
 }
 
-JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_resetMotionTracking(
-    JNIEnv*, jobject) {
-  app.TangoResetMotionTracking();
+JNIEXPORT jint JNICALL
+Java_de_stetro_master_constructnative_JNIInterface_initializeGLContent(
+    JNIEnv* /*env*/, jobject /*obj*/) {
+  return app.InitializeGLContent();
 }
 
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_initGlContent(
-    JNIEnv*, jobject) {
-  app.InitializeGLContent();
+Java_de_stetro_master_constructnative_JNIInterface_setRenderDebugPointCloud(
+    JNIEnv* /*env*/, jobject /*obj*/, jboolean on) {
+  app.SetRenderDebugPointCloud(on);
 }
 
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_setupGraphic(
-    JNIEnv*, jobject, jint width, jint height) {
+Java_de_stetro_master_constructnative_JNIInterface_setViewPort(
+    JNIEnv* /*env*/, jobject /*obj*/, jint width, jint height) {
   app.SetViewPort(width, height);
 }
 
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_render(
-    JNIEnv*, jobject) {
+Java_de_stetro_master_constructnative_JNIInterface_render(
+    JNIEnv* /*env*/, jobject /*obj*/) {
   app.Render();
 }
 
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_freeGLContent(
-    JNIEnv*, jobject) {
+Java_de_stetro_master_constructnative_JNIInterface_freeGLContent(
+    JNIEnv* /*env*/, jobject /*obj*/) {
   app.FreeGLContent();
 }
 
-JNIEXPORT jstring JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_getPoseString(
-    JNIEnv* env, jobject) {
-  return (env)->NewStringUTF(app.GetPoseString().c_str());
-}
-
-JNIEXPORT jstring JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_getEventString(
-    JNIEnv* env, jobject) {
-  return (env)->NewStringUTF(app.GetEventString().c_str());
-}
-
-JNIEXPORT jstring JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_getVersionNumber(
-    JNIEnv* env, jobject) {
-  return (env)->NewStringUTF(app.GetVersionString().c_str());
-}
-
 JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_setCamera(
-    JNIEnv*, jobject, int camera_index) {
-  using namespace tango_gl;
-  GestureCamera::CameraType cam_type =
-      static_cast<GestureCamera::CameraType>(camera_index);
-  app.SetCameraType(cam_type);
+Java_de_stetro_master_constructnative_JNIInterface_onTouchEvent(
+    JNIEnv* /*env*/, jobject /*obj*/, jfloat x, jfloat y) {
+  app.OnTouchEvent(x, y);
 }
 
-JNIEXPORT void JNICALL
-Java_com_projecttango_experiments_nativeaugmentedreality_TangoJNINative_onTouchEvent(
-    JNIEnv*, jobject, int touch_count, int event, float x0, float y0, float x1,
-    float y1) {
-  using namespace tango_gl;
-  GestureCamera::TouchEvent touch_event =
-      static_cast<GestureCamera::TouchEvent>(event);
-  app.OnTouchEvent(touch_count, touch_event, x0, y0, x1, y1);
-}
 #ifdef __cplusplus
 }
 #endif
